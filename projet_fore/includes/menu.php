8c2d8475078a22bc8daec4c675fc74e823ec5738
yvonne ney <marsouins2019@gmail.com>
2019-12-06 11:26:34 -0400

    06/12/2019

--- projet_fore/includes/menu.php
+++ projet_fore/includes/menu.php
@@ -73,13 +73,14 @@
 
 <div class="container-fluid contenU">
   <div class="row" >
-    
-
+ <img class="imgY" src="/projet_fore/images/découp.jpg" alt="">
+  
   </div>
+  
   <div class="row">
     <div class="col-6">
       <div class="contentL firstB" > 
-        <img class="imgT" src="/projet_fore/images/parcours-personnalise-dintegration.png" alt="">
+        <img class="imgz" src="/projet_fore/images/découp2.jpg" alt="">
       </div>
     </div>
 
@@ -88,20 +89,20 @@
         <h3> <strong>Le Parcours Personnalisé D’intégration Républicaine</strong> </h3>
         <br>
 
-        <h5 class="sousT" ><strong>Parcours personnalisé d’intégration républicaine</strong> </h5>
+        <h5 class="sousT" ><strong>-->Parcours personnalisé d’intégration républicaine</strong> </h5>
        
         <p>La loi du 7 mars 2016 relative au droit des étrangers en France réforme le dispositif d’accueil et d’intégration des étrangers accédant pour la première fois au séjour en France et désireux de s’y installer durablement. Elle renforce les outils d’intégration des étrangerslégalement admis en France.</p>
 <p> Plusieurs mesures sont entrées en vigueur le 1 er juillet 2016 pour garantir un meilleur accueil et favoriser l’intégration rapide des étrangers primo-arrivants qui s’engagent désormais dans un parcours
 personnalisé d’intégration républicaine d’une durée de 5 ans.</p>
 
-<h5 class="sousT"><strong>La préparation de l’arrivée en France</strong> </h5>
+<h5 class="sousT"><strong>-->La préparation de l’arrivée en France</strong> </h5>
 
 <p>Un livret d’informations, traduit en plusieurs langues, est accessibledès le pays d’origine sur l’ensemble des sites web du ministère del’intérieur, de l’Office français de l’immigration et de l’intégration
 (OFII), des ambassades et des consulats français. Ce livret recense l’ensemble des informations utiles aux étrangers pour préparer leurvenue en France : les valeurs et principes qui caractérisent la société
 française et l’ensemble des démarches administratives à accompliravant le départ et lors de l’installation en France.</p>
 
 
-<h5 class="sousT" ><strong>Le contrat d’intégration républicaine,socle d’engagement dans un parcours personnaliséd’intégration républicaine d’une durée de 5 ans</strong> </h5>
+<h5 class="sousT" ><strong>-->Le contrat d’intégration républicaine,socle d’engagement dans un parcours personnaliséd’intégration républicaine d’une durée de 5 ans</strong> </h5>
 
 <p>La signature d’un contrat d’intégration républicaine (CIR) entré en vigueur le 1 er juillet 2016 marque l’engagement de l’étranger dans leparcours personnalisé d’intégration républicaine. Le CIR se substitue au contrat d’accueil et d’intégration (CAI).</p>
 
@@ -114,15 +115,22 @@ française et l’ensemble des démarches administratives à accompliravant le d
   </div>
 
   <div class="row">
-    <div class="col-6">
+ 
+  <div class="col-6">
+  
       <div class="contentR" >
-        
-
       <h3> <strong>Le Parcours Personnalisé D’intégration Républicaine</strong> </h3>
         <br>
-      <h5 class="sousT"> <strong>Le parcours personnalisé d’intégration républicaine</strong> </h5>
-    <p></p>
-    <p></p>
+      <h5 class="sousT" > <strong>--> Une offre de service de droit commun mobilisée sur les territoires.</strong> </h5>
+        <p>Les préfets de région et de département déclinent au niveau local les orientations stratégiques nationales en matière 
+        d’intégration adressées chaque année par le ministre de l’intérieur. Ils assurent la structuration et l’accessibilité de 
+        l’offre de services pour mettre en œuvre les parcours d’intégration républicaine adaptés aux besoins des étrangers
+         primo-arrivants. Ils veillent à l’articulation des différents acteurs locaux (notamment associatifs) qui accompagnent
+          ce public particulier, à la complémentarité des actions et des financements.</p>
+
+        <p>Des actions nombreuses sont soutenues et conduites par les services de l’Etat sur le territoire.
+         Elles visent principalement l’apprentissage de la langue française par le biais d’ateliers socio-linguistiques, l’insertion sociale et professionnelle ainsi que l’accès aux droits.
+          Des travaux de cartographie ont été initiés pour permettre une meilleure visibilité de l’offre de formation linguistique et d’accès aux droits sur les territoires</p>
       <a id="red" rel="float" class="button hvr-grow hvr-icon-forward butH" href="#home">
         Plus d'info
         <i class="fa fa-chevron-circle-right hvr-icon"></i>
@@ -131,8 +139,9 @@ française et l’ensemble des démarches administratives à accompliravant le d
     </div>
 
     <div class="col-6">
-      <div class="contentI secondB" > 
-      <img class="imgA" src="https://ancrages.org/wp-content/uploads/2017/05/10042017-parcours-integration-republicaine-A3-VALIDEE-1.jpg" alt="">
+      <div class="contentI secondB" >
+      <img class="imgE" src="/projet_fore/images/découpage3.jpg" alt="">
+     
       </div>
     </div>
   </div>
@@ -140,7 +149,7 @@ française et l’ensemble des démarches administratives à accompliravant le d
   <div class="row">
     <div class="col-6">
       <div class="contentL firstB" > 
-        <img class="imgT" src="https://www.blogplm.com/wp-content/uploads/sites/5/2014/03/processus-projet.jpg" alt="">
+      <img class="imgR" src="/projet_fore/images/dcpg1.png" alt="">
       </div>
     </div>
 
@@ -148,9 +157,12 @@ française et l’ensemble des démarches administratives à accompliravant le d
       <div class="contentR" >
       <h3> <strong>Le Parcours Personnalisé D’intégration Républicaine</strong> </h3>
         <br>
-      <h5 class="sousT" > <strong>Le parcours personnalisé d’intégration républicaine</strong> </h5>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam ullam reiciendis dolore possimus ipsam architecto cum fugit mollitia, eius tempora nam quo nihil distinctio exercitationem doloremque asperiores quibusdam cupiditate blanditiis.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta itaque sequi debitis placeat. Perspiciatis similique laboriosam obcaecati aperiam temporibus facilis porro, mollitia corporis reiciendis cumque consectetur in delectus saepe provident.</p>
+      <h5 class="sousT" > <strong>--> L’acquisition d’un niveau supérieur de connaissance du français.</strong> </h5>
+        <p> La poursuite du parcours de formation linguistique doit permettre à l’étranger d’atteindre le niveau A2 de connaissance du français.
+            L’atteinte de ce niveau devient, à compter de mars 2018, une des conditions de délivrance de la carte de résident. 
+            Ce niveau permet à la France de se rapprocher des standards européens et de favoriser l’autonomie des étrangers dans notre société, et notamment l’accès à l’emploi et à la formation professionnelle.
+            Un second marché de formation linguistique de niveau A2 et B1 a été passé afin d’engager ces formations dès le dernier trimestre 2016.</p>        
+        <p>Par ailleurs, une plateforme numérique sera mise à la disposition des étrangers ainsi que des acteurs de l’intégration pour un accès à des formations civique et linguistique, à des informations pratiques, administratives et juridiques, et aux bonnes pratiques dans le domaine.</p>
       <a id="red" rel="float" class="button hvr-grow hvr-icon-forward butH" href="#home">
         Plus d'info
         <i class="fa fa-chevron-circle-right hvr-icon"></i>
@@ -164,9 +176,10 @@ française et l’ensemble des démarches administratives à accompliravant le d
       <div class="contentR" >
       <h3> <strong>Le Parcours Personnalisé D’intégration Républicaine</strong> </h3>
         <br>
-      <h5 class="sousT" > <strong>Le parcours personnalisé d’intégration républicaine</strong> </h5>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam ullam reiciendis dolore possimus ipsam architecto cum fugit mollitia, eius tempora nam quo nihil distinctio exercitationem doloremque asperiores quibusdam cupiditate blanditiis.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta itaque sequi debitis placeat. Perspiciatis similique laboriosam obcaecati aperiam temporibus facilis porro, mollitia corporis reiciendis cumque consectetur in delectus saepe provident.</p>
+      <h5 class="sousT" > <strong>--> L’accès à la nationalité française.</strong> </h5>
+        <p> Il consacre un parcours réussi d’intégration à la société française qui marque une volonté pour le demandeur de rejoindre la communauté nationale, de devenir citoyen français, d’adhérer aux valeurs et principes de la République française, 
+        de s’inscrire dans son histoire et sa culture et de l’enrichir par ses compétences et son histoire personnelle. L’assimilation à la communauté française est ainsi un critère essentiel dans l’examen de la demande de naturalisation.</p>        
+        
       <a id="red" rel="float" class="button hvr-grow hvr-icon-forward butH" href="#home">
         Plus d'info
         <i class="fa fa-chevron-circle-right hvr-icon"></i>
@@ -176,7 +189,7 @@ française et l’ensemble des démarches administratives à accompliravant le d
 
     <div class="col-6">
       <div class="contentI secondB" > 
-        <img class="imgT imgL" src="https://www.blogplm.com/wp-content/uploads/sites/5/2014/03/processus-projet.jpg" alt="">
+      <img class="imgT" src="/projet_fore/images/parcours-personnalise-dintegration.png" alt="">
       </div>
     </div>
   </div>
