7667de4bd77408376c4c0ba94a3bab1d00698237
yvonne ney <marsouins2019@gmail.com>
2020-02-10 15:58:09 -0400

    10/02/2020

--- mon_porfolio2/traitement.php
+++ mon_porfolio2/traitement.php
@@ -8,7 +8,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $mail = new PHPMailer();
 
